251d995ca35b005b547fd09da3d0e465816b5834
수빈 <soooh@subin-ui-MacBookPro.local>
2021-05-28 02:02:03 +0900

    0527

--- 02_push_swap/srcs/push_swap.c
+++ 02_push_swap/srcs/push_swap.c
@@ -6,21 +6,96 @@
 /*   By: soooh <soooh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 16:10:02 by soooh             #+#    #+#             */
-/*   Updated: 2021/05/26 19:35:44 by soooh            ###   ########.fr       */
+/*   Updated: 2021/05/28 01:28:20 by soooh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./push_swap.h"
 
-int			main(int argc, char **argv)
+t_node			*init_node(void)
 {
-	int		i;
-	int		save[argc];
+	t_node		*node;
 
-	i = -1;
-	while (++i < argc - 1)
+	node = (t_node *)malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->prev = NULL;
+	node->next = NULL;
+	node->data = 0;
+	return (node);
+}
+
+void			connect_node(t_node *temp, t_node **node, t_stack *a)
+{
+	if (!*node)
 	{
-		save[i] = ps_atoi(argv[i + 1]);
-		printf("%d\n", save[i]);
+		*node = temp;
+		a->head = temp;
 	}
+	else
+	{
+		(*node)->next = temp;
+		temp->prev = *node;
+		*node = (*node)->next;
+	}
+}
+
+int				make_node(char *argv, t_node **node, t_stack *a)
+{
+	t_node		*temp;
+
+
+	temp = init_node();
+	if (!temp)
+		return (0);
+	temp->data = ps_atoi(argv);
+	printf("argc = %d\n", temp->data);
+	connect_node(temp, node, a);
+	return (1);
+}
+
+void			set_stack(int argc, char **argv, t_stack *a)
+{
+	int			i;
+	int			stack;
+	t_node		*node;
+	t_node		*temp;
+
+	i = 0;
+	node = NULL;
+	while (++i < argc)
+	{
+		stack = make_node(argv[i], &node, a);
+		if (!stack)
+			ps_error("ERROR !\n");
+		a->total++;
+	}
+	temp = a->head;
+	while (temp)
+	{
+		printf("temp = %d\n", temp->data);
+		temp = temp->next;
+	}
+	printf("head = %d\n", ((t_node *)a->head)->data);
+	if (node->next == NULL)
+		a->tail = node;
+	printf("tail = %d\n", ((t_node *)a->tail)->data);
+	while(node->prev)
+		node = node->prev;
+}
+
+int				main(int argc, char **argv)
+{
+	t_stack		a;
+// 	int			i;
+// 	int			save[argc];
+// 
+// 	i = -1;
+// 	while (++i < argc - 1)
+// 	{
+// 		save[i] = ps_atoi(argv[i + 1]);
+// 		printf("%d\n", save[i]);
+// 	}
+	printf("main argc = %d\n", argc);
+	set_stack(argc, argv, &a);
 }
--- 02_push_swap/srcs/push_swap.h
+++ 02_push_swap/srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: soooh <soooh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 16:10:51 by soooh             #+#    #+#             */
-/*   Updated: 2021/05/26 20:52:50 by soooh            ###   ########.fr       */
+/*   Updated: 2021/05/27 20:39:39 by soooh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,25 @@
 # define INT_MAX	2147483647
 //INT_MIN = -INT_MAX -1
 
+typedef struct	s_node
+{
+	struct s_node	*prev;
+	struct s_node	*next;
+	int				data;
+}					t_node;
+
 typedef struct		s_stack
 {
-	struct s_stack	*head;
-	struct s_stack	*tail;
+	void			*head;
+	void			*tail;
 	int				total;
 }					t_stack;
-
-// typedef struct 		s_node
-// {
-// 	struct s_node	*prev;
-// 	struct s_node	*next;
-// 	int				data;
-// 	int				flag;
-// };
 
 /*
 ** ps_utils.c
 */
-void			ps_putstr(char *s);
-void			ps_error(char *text);
-int				ps_atoi(const char *str);
+void				ps_putstr(char *s);
+void				ps_error(char *text);
+int					ps_atoi(const char *str);
 
 #endif
